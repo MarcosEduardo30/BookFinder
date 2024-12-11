b414f12f672627ba764e032c6a1e045031572117
MarcosEduardo30 <marcos302k02@gmail.com>
2024-12-11 18:25:51 -0300

    Adicionado a barra de busca na tela inicial

---
+++ BookFinder/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookFinder.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search(string BookTitle)
+        {
+            ViewData["BookTitle"] = BookTitle;
+            return View();
+        }
+    }
+}
