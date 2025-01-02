4bdf2bd8c240c4f55ef2e7488ea89cc8d8b4514b
MarcosEduardo30 <marcos302k02@gmail.com>
2025-01-01 22:07:51 -0300

    Adicionado o layout da tela de pesquisa da página inicial

--- BookFinder/obj/Debug/net8.0/BookFinder.AssemblyInfo.cs
+++ BookFinder/obj/Debug/net8.0/BookFinder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+951f7702a5201a88bb5efe34631ed5861806fbcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7beb12c39c4772d79418ecb4a7d00965c9500067")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
