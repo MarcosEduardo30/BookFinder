9097fe9f279af6b82eeccd2c8367726985603e44
MarcosEduardo30 <marcos302k02@gmail.com>
2025-01-15 19:02:47 -0300

    Ajustes de responsividade para a página inicial

--- BookFinder/obj/Debug/net8.0/BookFinder.AssemblyInfo.cs
+++ BookFinder/obj/Debug/net8.0/BookFinder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8140946f1cd0d873749c3004621b03296a27514d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b24c30dac9905128ae28cc8b8bfe8809f4221c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
