75a8cd460b5c543f73b0f1a2387637003cacf59d
MarcosEduardo30 <marcos302k02@gmail.com>
2025-01-19 18:01:14 -0300

    Adicionado responsividade ao restante das páginas

--- BookFinder/wwwroot/js/site.js
+++ BookFinder/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function toggleNavBar() {
+    var nav = document.getElementById("navBar");
+    nav.classList.toggle("nav-Toggle");
+}
