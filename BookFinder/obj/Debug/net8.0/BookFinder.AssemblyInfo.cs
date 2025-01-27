11dccdd5dc11c85e04e0e012a7e8db2042eafeef
MarcosEduardo30 <marcos302k02@gmail.com>
2025-01-27 16:12:08 -0300

    Correção na responsividade da barra de pesquisas

--- BookFinder/obj/Debug/net8.0/BookFinder.AssemblyInfo.cs
+++ BookFinder/obj/Debug/net8.0/BookFinder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6c8bc03866b105152407066fe758c45d1e0eaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a62a98d53a3d92167c684636b7abb9f53e54c6b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
